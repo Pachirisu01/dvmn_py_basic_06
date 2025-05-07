27861875acd8baec6e1f520fe13427091dc87f9f
Polina Vasilyeva <Polinova31@gmail.com>
2025-05-07 14:53:45 +0300

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,30 @@
+password = input()
+pass_lenght = len(password)
+
+
+print('Введите пароль: ', password)
+
+
+def password_score_all(password):
+    score = 0
+    if any(letter.isdigit() for letter in password):
+        score += 2
+    if any(letter.isupper() for letter in password):
+        score += 2
+    if any(letter.islower() for letter in password):
+        score += 2
+    if any(not letter.isalpha() and not letter.isdigit() for letter in password):
+       	score += 2
+    if len(password) >= 12:  
+        score += 2
+    return score
+
+
+print('Рейтинг пароля:', password_score_all(password))
+
+
+
+
+
+
+
