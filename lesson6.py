ceb6aa006a092420ef065c05a1518ec71fc51755
Polina Vasilyeva <Polinova31@gmail.com>
2025-05-15 11:43:15 +0300

    Add files via upload

--- lesson6.py
+++ lesson6.py
@@ -1,28 +1,27 @@
-password = input()
-pass_lenght = len(password)
+PASSWORD = input()
 
 
-print('Введите пароль: ', password)
+print('Введите пароль: ', PASSWORD)
 
 
-def password_score_all(password):
+def password_score_all(PASSWORD):
     score = 0
-    if any(letter.isdigit() for letter in password):
+    if any(letter.isdigit() for letter in PASSWORD):
         score += 2
-    if any(letter.isupper() for letter in password):
+    if any(letter.isupper() for letter in PASSWORD):
         score += 2
-    if any(letter.islower() for letter in password):
+    if any(letter.islower() for letter in PASSWORD):
         score += 2
-    if any(not letter.isalpha() and not letter.isdigit() for letter in password):
+    if any(not letter.isalpha() and not letter.isdigit() for letter in PASSWORD):
        	score += 2
-    if len(password) >= 12:  
+    if len(PASSWORD) >= 12:  
         score += 2
     return score
 
 
 if __name__ == "__main__":
-	print('Рейтинг пароля:', password_score_all(password))
-	password_score_all(password)
+	print('Рейтинг пароля:', password_score_all(PASSWORD))
+	password_score_all(PASSWORD)
 
 
 
