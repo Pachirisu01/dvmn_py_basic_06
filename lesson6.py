09ff00d76782308c8f6c5aae8b525987ef76e20c
Polina Vasilyeva <Polinova31@gmail.com>
2025-05-19 13:10:12 +0300

    Add files via upload

--- lesson6.py
+++ lesson6.py
@@ -1,30 +1,23 @@
 
 def has_digit(password):
-    for letter in password:
-        if letter.isdigit():
-            return True
-    return False
+    return any(character.isdigit() for character in password)
+
 
 def has_letters(password):
-    for letter in password:
-        if letter.isalpha():
-            return True
-    return False
+    return any(character.isalpha() for character in password)
+
 
 def has_upper_letters(password):
-    for letter in password:
-        if letter.isupper():
-            return True
-    return False
+    return any(character.isupper() for character in password)
+
 
 def has_lower_letters(password):
-    for letter in password:
-        if letter.islower():
-            return True
-    return False
+    return any(character.islower() for character in password)
+
 
 def is_very_long(password):
     return len(password) >= 12
+
 
 def main():
     password = input("Введите пароль: ")
