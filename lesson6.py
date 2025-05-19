168f22c4e30b0c08226e2ecee95594d46490f8c1
Polina Vasilyeva <Polinova31@gmail.com>
2025-05-19 13:21:16 +0300

    Add files via upload

--- lesson6.py
+++ lesson6.py
@@ -22,19 +22,21 @@ def is_very_long(password):
 def main():
     password = input("Введите пароль: ")
     score = 0
-    if has_digit(password):
-        score += 2
-    if has_letters(password):
-        score += 2
-    if has_upper_letters(password):
-        score += 2
-    if has_lower_letters(password):
-        score += 2
-    if is_very_long(password):
-        score += 2
+
+    check_points= [
+        (has_digit, 2),
+        (has_letters, 2),
+        (has_upper_letters, 2),
+        (has_lower_letters, 2),
+        (is_very_long, 2)
+        ]
+
+    for check, points in check_points:
+        if check(password):
+            score += points 
+
     print('Рейтинг пароля : ', score)
     return score
-
 
 
 if __name__ == "__main__":
